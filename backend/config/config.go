3bfd229b0a5b7dde89a0c1e820341061135d85e1
rsmciftci <rsmciftci@gmail.com>
2024-03-16 10:02:31 +0000

    postgresql connection

---
+++ backend/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+var err error
+
+func DatabaseInit() {
+	host := "localhost"
+	user := "postgres"
+	password := "123456"
+	dbName := "auto_auction"
+	port := 5432
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Europe/London", host, user, password, dbName, port)
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func DB() *gorm.DB {
+	return db
+}
