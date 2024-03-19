03284eda37b72f8ddb147792c512dd10a8d8cbd8
rsmciftci <rsmciftci@gmail.com>
2024-03-19 14:27:12 +0000

    userSlice updated

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -12,38 +12,30 @@ const initialState = {
 
 }
 
-export const createAccountDrawerSlice = createSlice({
+export const userSlice = createSlice({
     name: "userSlice",
     initialState,
     reducers: {
-
-        setUser: (state,action) => {
-
-            state.ID = action.ID,
-            state.Name = action.Name,
-            state.Surname = action.Surname,
-            state.Email = action.Email,
-            state.Phone = action.Phone,
-            state.Auction = action.Auction,
-            state.Dob = action.Dob
-
+        setUser: (state, action) => {
+            state.ID = action.payload.ID;
+            state.Name = action.payload.Name;
+            state.Surname = action.payload.Surname;
+            state.Email = action.payload.Email;
+            state.Phone = action.payload.Phone;
+            state.Auction = action.payload.Auction;
+            state.Dob = action.payload.Dob;
         },
-     
-
         initializeUser: (state) => {
-
-            state.ID = null,
-            state.Name = null,
-            state.Surname = null,
-            state.Email = null,
-            state.Phone = null,
-            state.Auction = null,
-            state.Dob = null
-
+            state.ID = null;
+            state.Name = null;
+            state.Surname = null;
+            state.Email = null;
+            state.Phone = null;
+            state.Auction = null;
+            state.Dob = null;
         }
     }
 })
 
-
-export const { initializeUser, setUser } = userSlice.actions
-export default userSlice.reducer
+export const { initializeUser, setUser } = userSlice.actions;
+export default userSlice.reducer;
