712d7602bd82d809f905a374fd32e384ced40525
rsmciftci <rsmciftci@gmail.com>
2024-03-19 13:44:37 +0000

    updated

--- frontend/src/components/CreateAccount.js
+++ frontend/src/components/CreateAccount.js
@@ -5,16 +5,26 @@ import Form from 'react-bootstrap/Form';
 import { closeCreateAccountDrawer } from '../redux/createAccountDrawerSlice';
 import styles from './CreateAccount.module.css'
 import { useState } from 'react';
+import { toast, Slide, ToastContainer } from 'react-toastify'
+import userService from '../services/UserService';
 
 
 function CreateAccount() {
 
-  const [name,setName] = useState("");
-  const [surname,setSurname] = useState("");
-  const [dob,setDob] = useState("");
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
-  const [password1,setPassword1] = useState("");
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [dob, setDob] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password1, setPassword1] = useState("");
+
+  const user = {
+    name: name,
+    surname: surname,
+    dob: convertDateFormat(dob) + "T00:00:00Z",
+    email: email,
+    password: password
+  }
 
 
   const show = useSelector(state => state.persistedData.createAccountDrawerSlice.show)
@@ -22,6 +32,63 @@ function CreateAccount() {
 
   function close() {
     dispatch(closeCreateAccountDrawer())
+  }
+
+  function convertDateFormat(dateString) {
+    var parts = dateString.split('/');
+    var newDateString = parts[2] + '-' + parts[1] + '-' + parts[0];
+    return newDateString;
+  }
+
+  function toastErr(message) {
+    toast.error(message, {
+      position: "bottom-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      transition: Slide,
+    });
+
+  }
+
+  function toastSuccess() {
+    toast.success('Succesfully registered!', {
+      position: "bottom-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      transition: Slide,
+    });
+  }
+
+  function Dispatch() {
+    dispatch(closeCreateAccountDrawer())
+  }
+
+
+  function Register() {
+    if (password1 !== password) {
+
+      toastErr("Passwords doesn't match!");
+
+    } else {
+      userService.saveUser(user)
+        .then(function (response) {
+          toastSuccess();
+        })
+        .catch(function (error) {
+          toastErr("Something went wrong!");
+        });
+
+    }
   }
 
   return (
@@ -43,12 +110,12 @@ function CreateAccount() {
 
             <Form.Group className="mb-3" controlId="formBasicText">
               <Form.Label className={styles.formLabel}>Surname</Form.Label>
-              <Form.Control type="text" placeholder="Surname" value={surname} onChange={(e) => setSurname(e.target.value)}/>
+              <Form.Control type="text" placeholder="Surname" value={surname} onChange={(e) => setSurname(e.target.value)} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicText">
               <Form.Label className={styles.formLabel}>Date of Birth</Form.Label>
-              <Form.Control type="text" placeholder="DD/MM/YYYY" value={dob} onChange={(e) => setDob(e.target.value)}/>
+              <Form.Control type="text" placeholder="DD/MM/YYYY" value={dob} onChange={(e) => { setDob(e.target.value); }} />
             </Form.Group>
 
 
@@ -56,7 +123,7 @@ function CreateAccount() {
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label className={styles.formLabel}>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+              <Form.Control type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -65,11 +132,10 @@ function CreateAccount() {
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label className={styles.formLabel}>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password"  value={password1} onChange={(e) => setPassword1(e.target.value) }/>
+              <Form.Control type="password" placeholder="Password" value={password1} onChange={(e) => setPassword1(e.target.value)} />
             </Form.Group>
             <div className={styles.loginButtonDiv}>
-              {/* TODO: Login button functionality */}
-              <Button variant="primary">
+              <Button variant="primary" onClick={() => Register()}>
                 Register
               </Button>
             </div>
@@ -77,6 +143,7 @@ function CreateAccount() {
           </Form>
         </Offcanvas.Body>
       </Offcanvas>
+      <ToastContainer />
     </>
   );
 }
